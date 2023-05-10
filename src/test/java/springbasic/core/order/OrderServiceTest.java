2c8ce8e141e10712aa6d9545cdc1f47ca470ce0d
baamyumi <dbal5886@gmail.com>
2023-05-10 17:17:16 +0900

    AppConfig를 이용한 관심사 분리

--- src/test/java/springbasic/core/order/OrderServiceTest.java
+++ src/test/java/springbasic/core/order/OrderServiceTest.java
@@ -1,13 +1,22 @@
 package springbasic.core.order;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import springbasic.core.AppConfig;
 import springbasic.core.member.*;
 
 public class OrderServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
-    OrderService orderService = new OrderServiceImpl();
+    MemberService memberService;
+    OrderService orderService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderService();
+    }
 
     @Test
     void createOrder() {
