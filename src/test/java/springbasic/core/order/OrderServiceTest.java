ecd78273b9aa5d3e283d59675f796a31717e470d
baamyumi <dbal5886@gmail.com>
2023-05-10 13:56:20 +0900

    주문,할인 도메인

---
+++ src/test/java/springbasic/core/order/OrderServiceTest.java
@@ -0,0 +1,21 @@
+package springbasic.core.order;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import springbasic.core.member.*;
+
+public class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
